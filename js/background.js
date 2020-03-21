83dd73f78d9579e818f32502ffab6c168b349e93
Ihor Bodnarchuk <Ihor.Bodnarchuk@fsresidential.com>
2020-03-21 14:42:21 -0400

    replace tabs listener with webNavigation.onCompleted

--- js/background.js
+++ js/background.js
@@ -8,10 +8,7 @@
     }]);
   });
 
-  chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab){
-    if(tab.url && tab.url.indexOf('offerup.com/board/') > -1 && changeInfo.url === undefined) {
-        //we have a refresh
-        console.log("remove enableBoardItemSelectionMode storage data");
-        chrome.storage.sync.remove('enableBoardItemSelectionMode', _ => {});
-    }
-  });
+  //remove key on page load to reset the selector 
+  chrome.webNavigation.onCompleted.addListener(function() {
+    chrome.storage.sync.remove('enableBoardItemSelectionMode', _ => {});
+}, {url: [{urlPrefix : 'https://offerup.com/board/'}]});
