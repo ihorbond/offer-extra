f70ab17a8dff0ee15ba5ce94c95083fac728f52c
Ihor Bodnarchuk <Ihor.Bodnarchuk@fsresidential.com>
2020-04-02 21:40:03 -0400

    improve similar cars search

--- js/car_true_value.js
+++ js/car_true_value.js
@@ -1,192 +1,63 @@
-const productCatMetaTagContent = (document.head.querySelector("meta[property='product:category']").getAttribute("content") || '').toLowerCase();
-const isCarDetailPage = productCatMetaTagContent.indexOf("car") > -1 || productCatMetaTagContent.indexOf("truck") > -1;
-
 const keywords = {
-    "OUTOFSTOCK": "out of stock",
-    "IN": "in",
-    "DASH": "-"
+    "OUTOFSTOCK": "out of stock"
 };
 
 const conditions = {
     "USED": "used",
-    "DAMAGED": "damaged"
+    "DAMAGED": "damaged",
+    "REFURBISHED": "refurbished"
 };
 
-const key = atob('TDNsNng3aUpBY1h3Y0tGUFFqdkRWNEdlYkVPYUlDUWs=');
-let make = null;
-let model = null;
-let year = null;
-let trim = null;
-let lat = null;
-let lng = null;
-let mileage = null;
+const productCatMetaTagContent = (document.head.querySelector("meta[property='product:category']").getAttribute("content") || '').toLowerCase();
+const isCarDetailPage = productCatMetaTagContent.indexOf("car") > -1 || productCatMetaTagContent.indexOf("truck") > -1;
+const isDamaged = (document.head.querySelector("meta[property='product:condition']").getAttribute("content") || '').toLowerCase() === conditions.DAMAGED;
+const currentCar = {};
 
-if(isCarDetailPage) {
-    console.info("We are on car listing page");
-    const condition = getCondition();
+if (isCarDetailPage && !isDamaged) {
+    //console.info("We are on car listing page");
+    const pageTitle = document.title.toLowerCase().replace(/\W+/gi, '');
+    const forSaleIndex = pageTitle.indexOf('forsale');
+    currentCar.description = pageTitle.substring(0, forSaleIndex).trim();
+    currentCar.year = getYear(pageTitle);
+    currentCar.lat = document.head.querySelector("meta[property='place:location:latitude']").getAttribute("content");
+    currentCar.lng = document.head.querySelector("meta[property='place:location:longitude']").getAttribute("content");
+    currentCar.mileage = getMileage();
+    currentCar.price = getPrice();
 
-    if(condition === conditions.USED) {
-        const pageTitle = document.title.toLowerCase().replace(/\W+/gi, '');
-        const forSaleIndex = pageTitle.indexOf('forsale');
-        const desc = pageTitle.substring(0, forSaleIndex).trim();
-        year = getYear(pageTitle);
-        lat = document.head.querySelector("meta[property='place:location:latitude']").getAttribute("content");
-        lng = document.head.querySelector("meta[property='place:location:longitude']").getAttribute("content");
-        mileage = getMileage();
-        
-
-        //showAlternatives({a:1, b:2});
-        getMakeModelTrim(desc);
-    }
-
-    // const isSold = checkSold();
-    // const similarItems = getSimilarItemsJson();
-
-    // getZipCodeAsync(getZipCodeApiUrl(pageTitle)).then(zipCode => {
-    //     if(zipCode){
-    //         console.log(`Year: ${year}`, `MakeModel: ${makeAndModel}`, isSold, condition, mileage, zipCode);
-    //     }else {
-    //         getZipCodeAsyncGoogle(getGoogleMapUrl()).then(zipCode => {
-    //             console.log(`Year: ${year}`, `MakeModel: ${makeAndModel}`, isSold, condition, mileage, zipCode);
-    //         });
-    //     }
-    // })
+    if (currentCar.year && currentCar.mileage && currentCar.lat && currentCar.lng)
+        showAlternatives();
 }
 
-async function getMakeModelTrim(desc) {
-    const url = `https://marketcheck-prod.apigee.net/v1/search?api_key=${key}&rows=0&facets=make|0|200`;
-
-    await fetch(url)
-        .then(makesResponse => getMake(makesResponse, desc))
-        .then(make => getModel(make, desc))
-        .then(model => getTrim(make, model, year, desc))
-        .then(_ => getSimilarListings(50, conditions.USED, 'price', 'asc', true))
-        .then(json => showAlternatives(json))
-        .catch(err => console.error(err));
-}
-
-function showAlternatives(json) {
-    console.log(json);
-    json.current_car = current_car = {
-        price: getPrice(),
-        mileage: mileage
-    };
-    chrome.storage.local.set({'similarCars': json});
-
+function showAlternatives() {
+    chrome.storage.local.set({ 'currentCar': currentCar });
     const xpath = "//div[@data-test='price-label']";
     const priceLabelDiv = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
     const parent = priceLabelDiv.parentElement;
-    const myHr = document.createElement('hr');
-    const myLink = document.createElement('a');
-    // const spinner = htmlToElement(`
-    //     <div class="spinner-border text-success" role="status">
-    //         <span class="sr-only">Loading OfferExtra offers...</span>
-    //     </div>`);
-    myLink.innerText = "See cheaper vehicles with similar mileage nearby";
-    myLink.addEventListener('click', () => chrome.runtime.sendMessage({showSimilarCars: true}));
-    parent.insertBefore(myHr, priceLabelDiv.nextSibling);
-    parent.insertBefore(myLink, myHr);
-    //parent.insertBefore(spinner, myLink);
-}
+    const container = document.createElement('div');
+    container.innerHTML = `
+        <div id="spinner" class="spinner-grow spinner-grow-md text-success" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+        <a id="show-deals" class="text-success font-weight-bold text-md">Loading similar offers nearby...</a>
+        <hr>
+    `;
+    parent.insertBefore(container, priceLabelDiv.nextSibling);
+    setTimeout(_ => {
+        document.getElementById("spinner").hidden = true;
+        const link = document.getElementById("show-deals");
+        link.innerHTML = "💰 See cheaper vehicles with similar mileage nearby";
+        link.addEventListener('click', () => chrome.runtime.sendMessage({ showSimilarCars: true }));
+    }, 2500)
 
-async function getMake(res, desc) {
-    if(res.ok) {
-        const json = await res.json();
-        const match = json.facets.make.find(x => desc.indexOf(x.item.toLowerCase()) > -1);
-        if(match){
-            make = match.item;
-            return Promise.resolve(make);
-        }
-        else {
-            return Promise.reject("Make not found");
-        }
-    }
-
-    return Promise.reject(res);
-}
-
-async function getModel(make, desc) {
-    const url = `https://marketcheck-prod.apigee.net/v1/search?api_key=${key}&rows=0&make=${make}&facets=model|0|30`
-    const res = await fetch(url);
-    if(res.ok) {
-        const json = await res.json();
-        const match = json.facets.model.find(x => desc.indexOf(x.item.toLowerCase()) > -1);
-        if(match) {
-            model = match.item;
-            return Promise.resolve(model);
-        }
-        else {
-            return Promise.reject("Model not found");
-        }
-    }
-
-    return Promise.reject(res);
-}
-
-async function getTrim(make, model, year, desc) {
-    const url = `https://marketcheck-prod.apigee.net/v1/search?api_key=${key}&rows=0&car_type=used&make=${make}&model=${model}&year=${year}&facets=trim`;
-    const res = await fetch(url);
-    if(res.ok) {
-        const json = await res.json();
-        const match = json.facets.trim.find(x => desc.indexOf(x.item.toLowerCase()) > -1);
-        if(match) {
-            trim = match.item;
-            return Promise.resolve(trim);
-        }
-    }
-
-    return Promise.resolve(null);
-}
-
-async function getSimilarListings(radius, condition, sortBy, sortOrder, includeStats) {
-    let url = [`https://marketcheck-prod.apigee.net/v2/search/car/active`,
-                `?api_key=${key}`,
-                `&make=${make}`,
-                `&model=${model}`,
-                `&year=${year}`,
-                `&car_type=${condition}`,
-                `&start=0`,
-                `&rows=50`,
-                `&latitude=${lat}`,
-                `&longitude=${lng}`,
-                `&radius=${radius}`,
-                `&sort_by=${sortBy}`,
-                `&sort_order=${sortOrder}`]
-                .join('');
-    if(trim) url += `&trim=${trim}`;
-    if(includeStats) url += `&stats=miles,price,dom`;
-
-    const res = await fetch(url);
-    if(res.ok) {
-        const json = await res.json();
-        return Promise.resolve(json);
-    }
-
-    return Promise.reject(res);
-}
-
-function getSimilarItemsJson() {
-    try {
-        let scriptText = [...document.scripts].find(s => s.text.trim().startsWith("window.__OU_PROPS")).text;
-        const startIndex = text.text.indexOf('{');
-        scriptText = scriptText.substring(startIndex) + '\"}}}]}]}';
-        return JSON.parse(scriptText);
-    }
-    catch (r) {
-        return null;
-    }
 }
 
 function getYear(s) {
-    return s.match(/\d{4}/)[0];
+    const matches = s.match(/\d{4}/);
+    return matches ? matches[0] : null;
 }
 
 function checkSold() {
     return (document.head.querySelector("meta[property='product:availability']").getAttribute("content") || '').toLowerCase() === keywords.OUTOFSTOCK;
-}
-
-function getCondition() {
-    return (document.head.querySelector("meta[property='product:condition']").getAttribute("content") || '').toLowerCase();
 }
 
 function getPrice() {
@@ -195,16 +66,9 @@ function getPrice() {
 
 function getMileage() {
     const matches = document.body.innerText.match(/\d+\sMiles/);
-    return matches.length > 0 
-        ? matches[0].split(' ')[0] 
+    return matches && matches.length > 0
+        ? matches[0].split(' ')[0]
         : null;
-}
-
-function htmlToElement(html) {
-    var template = document.createElement('template');
-    html = html.trim(); 
-    template.innerHTML = html;
-    return template.content.firstChild;
 }
 
 // function getZipCodeApiUrl(s) {
@@ -214,7 +78,7 @@ function htmlToElement(html) {
 //     const location = s.substring(inIndex, dashIndex).trim().split(', ');
 //     const city = location[0];
 //     const state = location[1];
-//     const key = atob('anMtSWhPRjJDZklHRzU1czZBajJhcjdHZkZYRjgxOWN0TlU4YkpMd0tRdDhCaE1zTjQ1TEpQMGFsd29sMVoyRjR4TA==');
+
 //     return `https://www.zipcodeapi.com/rest/${key}/city-zips.json/${city}/${state}`;
 // }
 
--- js/similar_cars.js
+++ js/similar_cars.js
@@ -1,27 +1,41 @@
 const dealDescriptions = {
-    "NO_ANALYSIS_AVAILABLE": "NO ANALYSIS",
-    "BETTER_DEAL":"BETTER DEAL",
+    "NO_ANALYSIS": "NO ANALYSIS",
+    "BETTER_DEAL": "BETTER DEAL",
     "WORSE_DEAL": "WORSE DEAL"
+};
+
+const conditions = {
+    "USED": "used",
+    "DAMAGED": "damaged",
+    "REFURBISHED": "refurbished"
 };
 
 const greenTriangle = `
 <svg class="bi bi-triangle-fill triangle-green text-success" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-    <path fill-rule="evenodd" d="M7.022 1.566a1.13 1.13 0 011.96 0l6.857 11.667c.457.778-.092 1.767-.98 1.767H1.144c-.889 0-1.437-.99-.98-1.767L7.022 1.566z" clip-rule="evenodd"></path>
+<path fill-rule="evenodd" d="M7.022 1.566a1.13 1.13 0 011.96 0l6.857 11.667c.457.778-.092 1.767-.98 1.767H1.144c-.889 0-1.437-.99-.98-1.767L7.022 1.566z" clip-rule="evenodd"></path>
 </svg>`;
 
 const redTriangle = `
 <svg class="bi bi-triangle-fill triangle-red text-danger" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-    <path fill-rule="evenodd" d="M7.022 1.566a1.13 1.13 0 011.96 0l6.857 11.667c.457.778-.092 1.767-.98 1.767H1.144c-.889 0-1.437-.99-.98-1.767L7.022 1.566z" clip-rule="evenodd"></path>
+<path fill-rule="evenodd" d="M7.022 1.566a1.13 1.13 0 011.96 0l6.857 11.667c.457.778-.092 1.767-.98 1.767H1.144c-.889 0-1.437-.99-.98-1.767L7.022 1.566z" clip-rule="evenodd"></path>
 </svg>`;
 
-chrome.storage.local.get('similarCars', data => {
-    console.log(data);
-    if (Object.keys(data.similarCars).length > 0) {
-        printCards(data.similarCars.current_car, data.similarCars.listings);
-    }
+const key = atob('TDNsNng3aUpBY1h3Y0tGUFFqdkRWNEdlYkVPYUlDUWs=');
+let currentCar = {};
+
+chrome.storage.local.get('currentCar', data => {
+    //console.log(data);
+    currentCar = data.currentCar;
+    getMakeModelTrim();
 });
 
-function printCards(cc, listings) {
+function hideLoading(hidden) {
+    document.getElementById("loading-desc").hidden = hidden;
+}
+
+function printCards(json) {
+    hideLoading(true);
+
     const usdFormatter = new Intl.NumberFormat('en-US', {
         style: 'currency',
         currency: 'USD',
@@ -32,48 +46,49 @@ function printCards(cc, listings) {
     });
     const defImgSrc = chrome.runtime.getURL('../images/no_img.png');
     const container = document.getElementById('container');
-    listings.forEach(lst => container.appendChild(createCard(lst, cc, defImgSrc, usdFormatter, milesFormatter)));
+    json.listings.forEach(lst => container.appendChild(createCard(lst, currentCar, defImgSrc, usdFormatter, milesFormatter)));
 }
 
 function createCard(lst, cc, defImgSrc, usdFormatter, milesFormatter) {
-        const imgSrc = lst.media && lst.media.photo_links.length > 0 
-            ? lst.media.photo_links[0] 
-            : defImgSrc;
+    const imgSrc = lst.media && lst.media.photo_links.length > 0
+        ? lst.media.photo_links[0]
+        : defImgSrc;
 
-        let price = '';
-        let isLowerPrice = null;
+    let price = '';
+    let isLowerPrice = null;
 
-        if(lst.price > 0) 
-            price = usdFormatter.format(lst.price);
-        if(cc.price && lst.price && lst.price >= cc.price) {
-            price += redTriangle;
-            isLowerMiles = false;
-        }
-        else if(cc.price && lst.price && lst.price < cc.price) {
-            price += greenTriangle;
-            isLowerPrice = true;
-        }
+    if (lst.price > 0)
+        price = usdFormatter.format(lst.price);
+    if (cc.price && lst.price && lst.price > cc.price) {
+        price += redTriangle;
+        isLowerPrice = false;
+    }
+    else if (cc.price && lst.price && lst.price < cc.price) {
+        price += greenTriangle;
+        isLowerPrice = true;
+    }
 
-        let miles = '';
-        let isLowerMiles = null;
-        if(lst.miles > 0)
-            miles = milesFormatter.format(lst.miles);
-        if(cc.mileage && lst.miles && lst.miles >= cc.mileage) {
-            miles += redTriangle;
-            isLowerMiles = false;
-        }
-        else if(cc.mileage && lst.miles && lst.miles < cc.mileage) {
-            miles += greenTriangle;
-            isLowerMiles = true;
-        }
+    let miles = '';
+    let isLowerMiles = null;
 
-        const dealDesc = getDealDesc(cc.price, cc.mileage, lst.price, lst.miles);
-        const dealDescCss = getDealDescCss(dealDesc);
-        const cardContainer = document.createElement("div");
-        cardContainer.id = lst.id;
-        cardContainer.classList.add('card', 'mt-3', 'mb-3');
-        cardContainer.style.width = '18rem';
-        cardContainer.innerHTML = `
+    if (lst.miles > 0)
+        miles = milesFormatter.format(lst.miles);
+    if (cc.mileage && lst.miles && lst.miles > cc.mileage) {
+        miles += redTriangle;
+        isLowerMiles = false;
+    }
+    else if (cc.mileage && lst.miles && lst.miles < cc.mileage) {
+        miles += greenTriangle;
+        isLowerMiles = true;
+    }
+
+    const dealDesc = getDealDesc(cc.price, cc.mileage, lst.price, lst.miles);
+    const dealDescCss = getDealDescCss(dealDesc);
+    const cardContainer = document.createElement("div");
+    cardContainer.id = lst.id;
+    cardContainer.classList.add('card', 'mt-3', 'mb-3');
+    cardContainer.style.width = '18rem';
+    cardContainer.innerHTML = `
         <div class="${dealDescCss} text-white text-center" title="Compared to the listing on OfferUp">
             <h6 class="card-header-text">${dealDesc}</h6> 
         </div>
@@ -81,14 +96,14 @@ function createCard(lst, cc, defImgSrc, usdFormatter, milesFormatter) {
         <div class="card-body">
           <h5 class="card-title text-center">${lst.heading}</h5>
           <h6 class="card-subtitle mb-2 text-muted text-center">
-            <span class="${isLowerPrice !== null && isLowerPrice ? 'text-success': 'text-danger'}">${price}</span>
-            <span class="${isLowerMiles !== null && isLowerMiles ? 'text-success':'text-danger'}">${miles}</span>
+            <span class="${isLowerPrice !== null && isLowerPrice ? 'text-success' : 'text-danger'}">${price}</span>
+            <span class="${isLowerMiles !== null && isLowerMiles ? 'text-success' : 'text-danger'}">${miles}</span>
           </h6>
           <ul id="car-details">
             <li>Inventory type: ${lst.inventory_type || 'N/A'}</li>
             <li>Seller type: ${lst.seller_type || 'N/A'}</li>
-            <li>1 owner: ${lst.carfax_1_owner === undefined ? 'N/A' : lst.carfax_1_owner ? '✅':'❌'}</li>
-            <li>Clean title: ${lst.carfax_clean_title === undefined ? 'N/A' : lst.carfax_clean_title ? '✅':'❌'}</li>
+            <li>1 owner: ${lst.carfax_1_owner === undefined ? 'N/A' : lst.carfax_1_owner ? '✅' : '❌'}</li>
+            <li>Clean title: ${lst.carfax_clean_title === undefined ? 'N/A' : lst.carfax_clean_title ? '✅' : '❌'}</li>
             <li>Ext Color: ${lst.exterior_color || 'N/A'}</li>
             <li>Interior color: ${lst.interior_color || 'N/A'}</li>
           </ul>
@@ -100,18 +115,18 @@ function createCard(lst, cc, defImgSrc, usdFormatter, milesFormatter) {
             ${lst.dom} days
         </div>`;
 
-        return cardContainer;
+    return cardContainer;
 }
 
 function getDealDesc(originalPrice, originalMileage, listingPrice, listingMileage) {
     if (!originalPrice || !listingPrice || !originalMileage || !listingMileage)
-        return dealDescriptions.NO_ANALYSIS_AVAILABLE;
+        return dealDescriptions.NO_ANALYSIS;
     if (listingPrice < originalPrice && listingMileage < originalMileage)
         return dealDescriptions.BETTER_DEAL;
     if (listingPrice > originalPrice && listingMileage > originalMileage)
         return dealDescriptions.WORSE_DEAL;
 
-    return dealDescriptions.NO_ANALYSIS_AVAILABLE;
+    return dealDescriptions.NO_ANALYSIS;
 }
 
 function getDealDescCss(dealDesc) {
@@ -120,4 +135,105 @@ function getDealDescCss(dealDesc) {
         case dealDescriptions.WORSE_DEAL: return 'bg-danger';
         default: return 'bg-dark'
     }
+}
+
+function showManualEntry() {
+    hideLoading(true);
+    const container = document.getElementById("container");
+    container.innerHTML = `<div class="text-center">There was an error loading similar listings</div>`;
+}
+
+async function getMakeModelTrim() {
+    const url = `https://marketcheck-prod.apigee.net/v1/search?api_key=${key}&rows=0&facets=make|0|200`;
+    const radius = 50; //mi
+
+    await fetch(url)
+        .then(getMake)
+        .then(getModel)
+        .then(getTrim)
+        .then(_ => getSimilarListings(radius, conditions.USED, 'price', 'asc', false))
+        .then(printCards)
+        .catch(err => {
+            //console.error(err);
+            showManualEntry();
+        });
+}
+
+async function getMake(res) {
+    if (res.ok) {
+        const json = await res.json();
+        const match = json.facets.make.find(x => currentCar.description.indexOf(x.item.toLowerCase()) > -1);
+
+        //need 1s delay between api calls to stay within the rate limit
+        return new Promise((resolve, reject) => {
+            match ? setTimeout(_ => resolve(match.item), 1000) : reject("Make not found");
+        });
+    }
+
+    return Promise.reject(res);
+}
+
+async function getModel(make) {
+    currentCar.make = make;
+    const url = `https://marketcheck-prod.apigee.net/v1/search?api_key=${key}&rows=0&make=${currentCar.make}&facets=model|0|30`
+    const res = await fetch(url);
+    if (res.ok) {
+        const json = await res.json();
+        const match = json.facets.model.find(x => currentCar.description.indexOf(x.item.toLowerCase()) > -1);
+
+        return new Promise((resolve, reject) => {
+            match ? setTimeout(_ => resolve(match.item), 1000) : reject("Model not found");
+        });
+    }
+
+    return Promise.reject(res);
+}
+
+async function getTrim(model) {
+    currentCar.model = model;
+    const startPos = currentCar.year.length + currentCar.make.length + currentCar.model.length;
+    const trim = currentCar.description.substring(startPos);
+    if (trim.length > 0) {
+        const url = `https://marketcheck-prod.apigee.net/v1/search?api_key=${key}&rows=0&make=${currentCar.make}&model=${currentCar.model}&year=${currentCar.year}&facets=trim`;
+        const res = await fetch(url);
+        if (res.ok) {
+            const json = await res.json();
+            const match = json.facets.trim.find(x => trim === x.item.toLowerCase());
+            if (match) {
+                currentCar.trim = match.item;
+                return Promise.resolve(currentCar.trim);
+            }
+        }
+    }
+
+    return Promise.resolve(null);
+}
+
+async function getSimilarListings(radius, condition, sortBy, sortOrder, includeStats) {
+    let url = [
+        `https://marketcheck-prod.apigee.net/v2/search/car/active`,
+        `?api_key=${key}`,
+        `&make=${currentCar.make}`,
+        `&model=${currentCar.model}`,
+        `&year=${currentCar.year}`,
+        `&car_type=${condition}`,
+        `&start=0`,
+        `&rows=50`,
+        `&latitude=${currentCar.lat}`,
+        `&longitude=${currentCar.lng}`,
+        `&radius=${radius}`,
+        `&sort_by=${sortBy}`,
+        `&sort_order=${sortOrder}`
+    ].join('');
+    
+    if (currentCar.trim) url += `&trim=${currentCar.trim}`;
+    if (includeStats) url += `&stats=miles,price,dom`;
+
+    const res = await fetch(url);
+    if (res.ok) {
+        const json = await res.json();
+        return Promise.resolve(json);
+    }
+
+    return Promise.reject(res);
 }
